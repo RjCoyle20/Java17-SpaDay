f3b28742d79c7300fa5278d92b63ed11b46290f6
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2023-10-10 19:21:40 -0500

    Started Studio ch 15, need to switch branches

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,15 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+
     private String email;
+    @NotBlank
     private String password;
 
     public User() {
