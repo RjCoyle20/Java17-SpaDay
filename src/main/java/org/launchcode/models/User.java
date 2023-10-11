f58f07007d79d27343a490ffe87fa1e316c0c03e
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2023-10-10 20:07:22 -0500

    Finished Studio ch 15 with bonus mission.

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,26 +1,33 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class User {
-    @NotBlank
-    @Size(min = 5, max = 15)
+
+    @Size(min = 5, max = 15, message = "Username must be 5-15 characters long.")
     private String username;
 
+    @Email(message = "Invalid email format.")
+    @Size(min = 0)
     private String email;
-    @NotBlank
-    private String password;
 
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
+    private String password;
+    @NotNull(message = "Passwords do not match AT ALL.")
+    private String verifyPassword;
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword= verifyPassword;
     }
 
     public String getUsername() {
@@ -45,6 +52,23 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    public void checkPassword(){
+        if (password != null && verifyPassword != null && !this.password.equals(this.verifyPassword)){
+            setVerifyPassword(null);
+        }
     }
 }
+
 
